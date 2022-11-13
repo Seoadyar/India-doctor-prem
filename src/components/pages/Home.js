958be967fbb2f8ddefb2064fe5c459f405ac2787
Karthik <karthik5112001@gmail.com>
2022-11-13 19:09:20 +0530

    commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -85,13 +85,7 @@ function Home() {
                 </video>
             </div>
 
-              <div className='spec_stock_img_cont'>
-                <div className='spec_stock_img_main_cont'>
-                    <img src={spec1}/>
-                    <img src={spec2}/>
-                    <img src={spec3}/>
-                </div>
-            </div>
+            
 
 
 
@@ -168,8 +162,8 @@ function Home() {
             <div className='spec_stock_img_cont'>
                 <div className='spec_stock_img_main_cont'>
                     <img src={spec1}/>
-                    <img src={spec2}/>
-                    <img src={spec3}/>
+                    {/* <img src={spec2}/>
+                    <img src={spec3}/> */}
                 </div>
             </div>
 
